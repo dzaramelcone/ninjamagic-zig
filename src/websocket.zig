069d29a611d0204b4717d28c219be14e084a7f73
Dzara Melcone <dzaramelcone@gmail.com>
2025-07-04 19:04:33 -0400

    ♻️ websocket.zig

---
+++ src/websocket.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const ws = @import("websocket");
+const cfg = @import("config.zig").Config;
+
+const App = struct {};
+const WsHandler = struct {
+    app: *App,
+    conn: *ws.Conn,
+    pub fn init(_: *const ws.Handshake, conn: *ws.Conn, app: *App) !WsHandler {
+        return .{
+            .app = app,
+            .conn = conn,
+        };
+    }
+
+    pub fn clientMessage(self: *WsHandler, data: []const u8) !void {
+        try self.conn.write(data); // echo the message back
+    }
+};
+
+pub fn runWsServer(alloc: std.mem.Allocator) !void {
+    // start the ws_server. TODO: move this into tardy if possible
+    var ws_server = try ws.Server(WsHandler).init(alloc, cfg.Ws);
+    defer ws_server.deinit();
+
+    var app = App{};
+    try ws_server.listen(&app);
+}
