533522b77b2dc36218a5cb57ee5fe25a03763ed4
Dzara Melcone <dzaramelcone@gmail.com>
2025-07-18 22:04:38 -0400

    main uses generic ws host

--- src/main.zig
+++ src/main.zig
@@ -3,43 +3,25 @@ const net = @import("net");
 const db = @import("db.zig");
 const State = @import("state.zig").State;
 const cfg = @import("core").Config;
-var state: *State = undefined;
 
-const EncloseState = struct {
-    pub fn onPacket(id: usize, txt: []u8) void {
-        state.onPacket(id, txt);
-    }
-    pub fn onConnect(id: usize, c: *net.Conn) void {
-        state.onConnect(id, c);
-    }
-    pub fn onClose(id: usize) void {
-        state.onClose(id);
-    }
-};
 pub fn main() !void {
     var gpa = std.heap.DebugAllocator(.{ .thread_safe = true }){};
-    const alloc = gpa.allocator();
+    const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    try db.doQueries(alloc);
+    // try db.doQueries(alloc);
 
-    var http = try std.Thread.spawn(.{}, net.host_http, .{alloc});
+    var http = try std.Thread.spawn(.{}, net.host_http, .{allocator});
     defer http.join();
 
-    state = try State.init(alloc);
-    defer state.deinit();
-    var deps = net.Deps{
-        .alloc = alloc,
-        .pushInbound = EncloseState.onPacket,
-        .registerClient = EncloseState.onConnect,
-        .unregister = EncloseState.onClose,
-    };
-    var ws = try std.Thread.spawn(.{}, net.host_ws, .{&deps});
+    const state = try State.init(allocator);
+
+    var ws = try std.Thread.spawn(.{}, net.host_ws, .{ State, allocator, state });
     defer ws.join();
     const tick: f64 = 1.0 / cfg.tps;
     const tick_ns = @as(u64, @intFromFloat(tick * 1e9));
     while (true) {
-        state.step(tick);
+        try state.step(tick);
         std.Thread.sleep(tick_ns);
     }
 }
