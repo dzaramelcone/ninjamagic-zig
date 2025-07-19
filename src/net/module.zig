5f2a012f395792a247567166af9259edbb5aedb8
Dzara Melcone <dzaramelcone@gmail.com>
2025-07-18 22:04:50 -0400

    remove Deps from net module

--- src/net/module.zig
+++ src/net/module.zig
@@ -2,5 +2,4 @@ pub const host_ws = @import("websocket.zig").host;
 pub const host_http = @import("web.zig").host;
 const ws = @import("websocket");
 
-pub const Deps = @import("websocket.zig").Deps;
 pub const Conn = ws.Conn;
