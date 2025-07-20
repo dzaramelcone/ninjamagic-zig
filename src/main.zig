df12ab1cf9e7fa664c4104b7f9799af74e896c65
Dzara Melcone <dzaramelcone@gmail.com>
2025-07-19 21:14:21 -0400

    thrash alloc name in main.zig for no reason

--- src/main.zig
+++ src/main.zig
@@ -11,17 +11,17 @@ pub fn main() !void {
     std.log.info(zts.s(tmpl, "foo"), .{"daytime"});
     std.log.info(zts.s(tmpl, "bar"), .{"nighttime"});
     var gpa = std.heap.DebugAllocator(.{ .thread_safe = true }){};
-    const allocator = gpa.allocator();
+    const alloc = gpa.allocator();
     defer _ = gpa.deinit();
 
     // try db.doQueries(alloc);
 
-    var http = try std.Thread.spawn(.{}, net.host_http, .{allocator});
+    var http = try std.Thread.spawn(.{}, net.host_http, .{alloc});
     defer http.join();
 
-    const state = try State.init(allocator);
+    const state = try State.init(alloc);
 
-    var ws = try std.Thread.spawn(.{}, net.host_ws, .{ State, allocator, state });
+    var ws = try std.Thread.spawn(.{}, net.host_ws, .{ State, alloc, state });
     defer ws.join();
     const tick: f64 = 1.0 / cfg.tps;
     const tick_ns = @as(u64, @intFromFloat(tick * 1e9));
