11ad0c2a241d8b9514eb8dff4443e99df35db42c
Dzara Melcone <dzaramelcone@gmail.com>
2025-08-03 20:51:20 -0400

    parse sends signals on error

--- src/state.zig
+++ src/state.zig
@@ -58,27 +58,10 @@ pub const State = struct {
     }
 
     pub fn onMessage(self: *State, user: usize, msg: []const u8) !void {
-        const sig = sys.parser.parse(.{ .user = user, .text = msg }) catch |err| {
-            handleParseError(user, err);
-            return;
-        };
+        const sig = sys.parser.parse(.{ .user = user, .text = msg }) catch return;
         if (!self.channel.push(sig)) return error.ServerBacklogged;
     }
-    fn handleParseError(user: usize, err: sys.parser.ParseError) void {
-        switch (err) {
-            error.NothingSent => return,
-            else => core.bus.enqueue(
-                .{
-                    .Outbound = .{
-                        .Message = .{ .to = user, .text = sys.parser.toPlayer(err) },
-                    },
-                },
-            ) catch |appendError| switch (appendError) {
-                error.Full => return,
-                else => unreachable,
-            },
-        }
-    }
+
     pub fn onConnect(self: *State, id: usize, c: *ws.Conn) !void {
         try self.conns.put(id, c);
     }
