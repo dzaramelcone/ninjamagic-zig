79e5032b126b54e3829f755613f88e7eb9b6a6cc
Dzara Melcone <dzaramelcone@gmail.com>
2025-08-04 21:02:11 -0400

    centralizes id gen for entities

--- src/core/util.zig
+++ src/core/util.zig
@@ -1,4 +1,13 @@
+const std = @import("std");
+
+var next_id: std.atomic.Value(usize) = .{ .raw = 1 };
+
+pub fn getId() usize {
+    return next_id.fetchAdd(1, .monotonic);
+}
+
 pub const Seconds = f64;
+
 pub const Cardinal = enum {
     north,
     northeast,
