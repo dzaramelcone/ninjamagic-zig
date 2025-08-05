55552c1fa44687cc1f238246f06f8e1be348fed0
Dzara Melcone <dzaramelcone@gmail.com>
2025-08-04 21:00:39 -0400

    adds some tests to client

--- src/sys/client.zig
+++ src/sys/client.zig
@@ -1,28 +1,63 @@
 const std = @import("std");
 const core = @import("core");
-const ws = @import("ws");
-
+const ws = @import("websocket");
 var clients: std.AutoArrayHashMap(usize, *ws.Conn) = undefined;
 
-pub fn init(alloc: std.mem.Allocator) !void {
-    clients.init(alloc);
+pub fn init(alloc: std.mem.Allocator) void {
+    clients = std.AutoArrayHashMap(usize, *ws.Conn).init(alloc);
 }
+
 pub fn deinit() void {
     clients.deinit();
 }
+
 pub fn step() void {
     var connects = core.bus.connect.flush() catch return;
     while (connects.next()) |sig| {
         const connect = sig.*;
-        clients.put(connect.source, connect.conn);
+        clients.put(connect.source, connect.conn) catch @panic("OOM");
     }
     var disconnects = core.bus.disconnect.flush() catch return;
     while (disconnects.next()) |sig| {
         const disconnect = sig.*;
-        clients.swapRemove(disconnect.source);
+        _ = clients.swapRemove(disconnect.source);
     }
 }
 
 pub fn list() []usize {
     return clients.keys();
+}
+
+test "basic client tracking" {
+    init(std.testing.allocator);
+    defer deinit();
+    try std.testing.expect(list().len == 0);
+    var mockConn = ws.Conn{
+        ._closed = false,
+        .started = 0,
+        .stream = undefined,
+        .address = undefined,
+    };
+
+    try core.bus.enqueue(.{ .Connect = .{ .source = 1, .conn = &mockConn } });
+    step();
+    try std.testing.expect(clients.get(1) != null);
+    try std.testing.expect(clients.get(2) == null);
+    try std.testing.expect(list().len == 1);
+
+    try core.bus.enqueue(.{ .Connect = .{ .source = 2, .conn = &mockConn } });
+    step();
+    try std.testing.expect(clients.get(1) != null);
+    try std.testing.expect(clients.get(2) != null);
+    try std.testing.expect(list().len == 2);
+
+    try core.bus.enqueue(.{ .Disconnect = .{ .source = 1 } });
+    step();
+    try std.testing.expect(clients.get(1) == null);
+    try std.testing.expect(clients.get(2) != null);
+    try std.testing.expect(list().len == 1);
+
+    try core.bus.enqueue(.{ .Disconnect = .{ .source = 2 } });
+    step();
+    try std.testing.expect(list().len == 0);
 }
